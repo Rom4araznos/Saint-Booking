d30e9bd364b57c756b507e63e8551ce974533f72
Mandrusov Roman <mandrusov2005@mail.ru>
2025-02-04 20:24:04 +0300

    Add functions for cryptography

---
+++ server/include/crypto.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <string>
+#include <sys/stat.h>
+
+namespace crypto {
+
+    auto salt() -> std::string;
+    auto hash_to_hex(const std::string &data) -> std::string;
+    auto create_hash_evp(const EVP_MD *mod,
+                         const std::string &data) -> std::optional<std::string>;
+
+    const std::string alph = "0123456789abcdef";
+    const std::string pepper = "9ad";
+
+} // namespace crypto
